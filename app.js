eab4eb3b74de992414a0a4c48b0ac30b862ef8f3
Akash Shah <aks301190@gmail.com>
2022-07-10 17:23:52 +0530

    Comment non required files

--- app.js
+++ app.js
@@ -21,24 +21,24 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.log(`DB Error: ${err.message}`));
 
 // middlewares
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 // app.use(cookieParser());
 app.use(expressValidator());
 
 // routes
 app.get('/', (req, res) => res.json(require('./docs/api')));  // localhost:8080/
-const postRoutes = require('./routes/postRoutes');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-app.use('/', postRoutes);
-app.use('/', authRoutes);
-app.use('/', userRoutes);
+// const postRoutes = require('./routes/postRoutes');
+// const authRoutes = require('./routes/authRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// app.use('/', postRoutes);
+// app.use('/', authRoutes);
+// app.use('/', userRoutes);
 
 // error handling
 app.use((err, req, res, next) => {    // see express-jwt docs
   if (err.name === 'UnauthorizedError') res.status(401).json({error: 'Unauthorized!'});
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`RD03-02-NodeAPI listening on port ${port}...`));
